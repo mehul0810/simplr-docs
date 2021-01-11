390460cc6976a94f02524491bbc8b7db9fec3adf
Mehul Gohil <mehul.gohil0810@gmail.com>
2021-01-11 19:10:14 +0530

    add empty actions and filters file

---
+++ src/Admin/Filters.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * SimplrDocs - Admin Filters.
+ *
+ * @package WordPress
+ * @subpackage SimplrDocs
+ * @since 1.0.0
+ */
+
+namespace SimplrDocs\Admin;
+
+// Bailout, if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+class Filters {
+
+	/**
+	 * Constructor.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 *
+	 * @return void
+	 */
+	public function __construct() {
+
+	}
+}
