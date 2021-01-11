2e72202c6d548832600cf1000ce03f27abb72d76
Mehul Gohil <mehul.gohil0810@gmail.com>
2021-01-11 19:16:59 +0530

    add settings admin menu

--- src/Admin/Actions.php
+++ src/Admin/Actions.php
@@ -25,6 +25,44 @@ class Actions {
 	 * @return void
 	 */
 	public function __construct() {
+		add_action( 'admin_menu', [ $this, 'register_menu' ] );
+	}
 
+	/**
+	 * Register Menu's.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 *
+	 * @return void
+	 */
+	public function register_menu() {
+		// Register Settings Page.
+		add_submenu_page(
+			'edit.php?post_type=simplrdocs_docs',
+			esc_html__( 'Settings', 'simplrdocs' ),
+			esc_html__( 'Settings', 'simplrdocs' ),
+			'manage_options',
+			'simplrdocs_settings',
+			[ $this, 'render_settings_page' ]
+		);
+	}
+
+	/**
+	 * Render Settings Page.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 *
+	 * @return void
+	 */
+	public function render_settings_page() {
+		ob_start();
+		?>
+		<div class="wrap">
+			<h1><?php esc_html_e( 'Settings', 'simplrdocs' ); ?></h1>
+		</div>
+		<?php
+		echo ob_get_clean();
 	}
 }
